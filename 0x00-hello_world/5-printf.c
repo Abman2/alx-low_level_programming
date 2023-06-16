9386b7eefaea4c35db5c643d9d030e0b2c3fe84c
Abdulrahman <remaxi135@gmail.com>
2023-06-15 21:38:50 -0700

    learning

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+*main - using to pring words using printing using printf
+* Return: 0 (Success)
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
