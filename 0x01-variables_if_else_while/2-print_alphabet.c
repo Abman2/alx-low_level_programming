652c45eac2c5f333f22ccf235504b8bce8fac508
Abdulrahman <remaxi135@gmail.com>
2023-06-17 09:00:05 -0700

    learning on conditional statement

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - printing alphabet
+* Return: 0
+*/
+
+int main(void)
+{
+for (char i  = 'a' ; i <= 'z' ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+}
+
