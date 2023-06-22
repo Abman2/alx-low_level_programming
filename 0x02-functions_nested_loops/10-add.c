c1a5b317f4689a0a9b6641282bd18749dda2ecfc
Abdulrahman <remaxi135@gmail.com>
2023-06-22 01:10:05 -0700

    nested

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * add - adding up 2 integers
+ * @i: doc line 1
+ * @j: doc line 2
  * Return: i + j
  */
 int add(int i, int j)
 {
 	int sum = i + j;
 
-	return sum;
+	return (sum);
 }
