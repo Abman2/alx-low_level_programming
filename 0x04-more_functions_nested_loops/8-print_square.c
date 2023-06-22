2c88238d9e16d4d8a789caf92b0efe51c49ad20c
Abdulrahman <remaxi135@gmail.com>
2023-06-22 14:39:22 -0700

    gee

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *print_square - draws square
+ *
+ *@size: int type parameter
+ *Return: prints diagonal line
+ */
+void print_square(int size)
+{
+if (n > 0) 
+{
+	int i;
+	int j;
+
+for (i = 0 ; i <= 9 ; i++)
+{
+for (j = 0 ; j <= 9 ; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+
+}
+
+
+}
+
+_putchar('\n')
+}
+
