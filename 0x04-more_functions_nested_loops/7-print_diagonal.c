41e567bc731c9031c902668ba451668ef0869b59
Abdulrahman <remaxi135@gmail.com>
2023-06-22 05:12:56 -0700

    gee

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * 
+  *
+  *
+  *\
+  void print_line(int n){
+ if (n <= 0){
+_putchar('\n');
+
+    }
+    
+    
+    else if (n > 0){
+        for (int i = 1; i <= n; i++){
+         if (i != n){ _putchar('\\');}
+          else if (i == n){
+              _putchar('\\');
+          }
+                    }
+                        _putchar('\n');
+    }
+
+  }
