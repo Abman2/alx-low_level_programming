a767835fc020327aded4674e48335a5a4c82b95d
Abman2 <remaxi135@gmail.com>
2023-06-22 08:09:23 +0100

    nested

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adding up 2 integers
+ * Return: i + j
+ */
+int add(int i, int j)
+{
+	int sum = i + j;
+	_putchar("%d\n", sum);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_to_98 - to print all natural numbers
+ * Return: return n!
+ */
+void print_to_98(int n){
+
+if (n < 98) 
+{
+_putchar(n..98);
+}
+
+
+}
