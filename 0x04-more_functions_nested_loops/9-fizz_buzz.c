6428fbe0d16824ffbac587e5a107fbce7fa72759
Abdulrahman <remaxi135@gmail.com>
2023-06-22 16:52:34 -0700

    gee

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,19 +16,13 @@ printf("Fizz");
 }
 else if (i % 5 == 0)
 {
-
-
-
-
 printf("Buzz");
 
 }
 else if ((i % 3 == 0) && (i % 5 == 0))
 {
-
 printf("FizzBuzz");
 }
-
 else
 {
 printf("%d", i);
@@ -37,9 +31,9 @@ if (i != 100)
 {
 	printf(" ");
 }
+
 }
 printf("\n");
-
 return (0);
 }
 
