160c5ae92b7c96c9da87eb346a3d1afc2b7db723
Abdulrahman <remaxi135@gmail.com>
2023-06-23 14:49:21 -0700

    gee

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,6 +9,20 @@
 
 int main(void)
 {
-	printf("6857\n");
+	int b;
+
+	long num = 612852475143;
+
+	for (b = (int) sqrt(num); b > 2; b++)
+	{
+		if (num % b == 0)
+		{
+			printf("%d\n", b);
+			break;
+		}
+
+	}
+
 	return (0);
+
 }
