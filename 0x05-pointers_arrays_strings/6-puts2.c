962fdfb6859938bf6b29efc0433cc22d0b35d3a8
Abdulrahman <remaxi135@gmail.com>
2023-06-26 12:33:28 -0700

    abman

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* puts2  - function of a function
+* _puts: heh
+* Return: 0
+* @str: integer
+*/
+void puts2(char *str)
+{
+int i, j;
+
+for (i = 0; i >= 0; i++)
+{
+if (s[i] == '\0')
+{
+break;
+}
+}
+
+for (j = 0; j <= (i - 1); j + 2)
+{
+_putchar(s[j]);
+}
+_putchar('\n');
+}
