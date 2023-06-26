b7bf71e2b5891e1fd9450dc7cfd5c42a2c87f932
Abdulrahman <remaxi135@gmail.com>
2023-06-26 10:18:30 -0700

    abman

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /*
-* _puts - function
+* _puts - function of a function
+* _puts: heh
+* Return: 0
 * @str: integer
 */
 void _puts(char *str)
@@ -13,7 +15,7 @@ if (str[i] == '\0')
 {
 break;
 }
-_putchar(i);
+_putchar(str[i]);
 }
 
 
