0b1df998c8c73fa0c0b9bb187f28a501804a3bf5
Abdulrahman <remaxi135@gmail.com>
2023-06-26 05:49:43 -0700

    abman

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,9 @@
 #include "main.h"
 /**
-*
-*
+* reset_to_98 - printing
+* @n: integer
 */
 void reset_to_98(int *n)
 	{
 		*n = 98;
-		
-
-
-
-
 	}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* swap_int - doings
+* @a: integer
+* @b: integer
+*/
+void swap_int(int *a, int *b)
+{
+
+*a = *b;
+*b = *a;
+
+
+}
