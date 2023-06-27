cb2c289e008d58eec5a6c11446d829833adc428a
Abdulrahman <remaxi135@gmail.com>
2023-06-26 20:48:05 -0700

    abman

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - converts a string to an integer
+ * @s: string to be converted
+ * @n: integer
+ * Return: integers in a string
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i <= n; i++)
+{
+if (i != '\0')
+{
+_putchar(',');
+_putchar(' ');
+
+}
+_putchar(a[i]);
+}
+_putchar('\n')
+
+}
