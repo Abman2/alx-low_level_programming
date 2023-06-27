fef25ba869c56547d265e34e56bc7c01a4a4d6cf
Abman2 <remaxi135@gmail.com>
2023-06-27 03:52:43 +0100

    abman

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* puts_half  - function of a function
+* _puts: heh
+* Return: 0
+* @str: integer
+*/
+void puts_half(char *str)
+{
+int i, j;
+
+
+for (i = 0; i >= 0; i++)
+{
+if (str[i] == '\0')
+{
+break;
+}
+}
+
+for (j = ((i - 1) / 2); j >= (i - 1); j++)
+{
+if (i % 2 == 0)
+{
+j = (i - 2) / 2;
+
+}
+_putchar(str[j]);
+}
