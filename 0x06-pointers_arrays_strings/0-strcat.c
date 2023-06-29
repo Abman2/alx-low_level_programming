7b5c22205e2daa891b6ac3efc909db8aef197da9
Abdulrahman <remaxi135@gmail.com>
2023-06-29 02:03:25 -0700

    testing

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,12 +2,22 @@
 /**
 * _strcat - function
 * @dest: parameter
-* @scr: parameter
+* @src: parameter
+* Return: dest
 */
 char *_strcat(char *dest, char *src)
 	{
-		
+		int i, j;
 
+		for (i = 0; src[i] != '\0' ; i++)
+		{
+		}
+		for (j = 0; dest[j] != '\0' ; j++)
+		{
+		}
+		dest[j] += src[i];
+
+		return (dest);
 
 
 
