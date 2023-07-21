6c879edd1cf1a15e11604b24d4050025e22ba682
Abdulrahman <remaxi135@gmail.com>
2023-07-21 15:33:18 -0700

    abman

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,31 +1,23 @@
+#include <stddef.h>
+#include <stdio.h>
 #include "function_pointers.h"
 /**
- * array_iterator - function
- * @array: parameter
- * @size: size of the array
- * @action: function pointer as a parameter
+ * int_index - it prints each array elem on a newl
+ * @array: array
+ * @size: how many elem to print
+ * @cmp: pointer to print in regular or hex
+ * Return: void
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-if(size <= 0) 
-{
-return (-1);
-}
-int i, temp;
+	int i;
 
-for(i = 0; i <= size; i++)
-{
-cmp(array[i]);
-array[i] = temp;
-if (array[i] == temp)
-{
-return (i);
-break;
-}
-else
-{
-return (-1);
-}
-}
-return (i);
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
 }
