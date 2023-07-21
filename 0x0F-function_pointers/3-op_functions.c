351ce18f12689f2f4435354a9bbafc860bd0dd38
Abdulrahman <remaxi135@gmail.com>
2023-07-21 16:54:40 -0700

    abman

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+/**
+* int op_add - returns addition
+* int op_sub - returns subtract
+* int op_mul - returns multiply
+* int op_div - returns division
+* int op_mod - returns modulus
+* @a: integer
+* @b: integer
+*/
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+int op_div(int a, int b)
+{
+return (a / b);
+}
+int op_mod(int a, int b)
+{
+return (a % b);
+}
