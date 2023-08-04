2033deda21ec36e9dd7be15bf9e58c58ef399b71
Abdulrahman <remaxi135@gmail.com>
2023-08-04 03:29:41 -0700

    "no1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* print_binary - function to convert binary to decimal
+* @n: argument in the function
+* Return: returns value
+*/
+
+void print_binary(unsigned long int n)
+{
+
+}
