1d87f8133e9c29b124321b7adfc4c5c7d13ece25
abman2 <remaxi135@gmail.com>
2023-09-04 14:16:17 -0700

    aa

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,22 +1,14 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * flip_bits - Returns the number of bits you would need to flip to get from
- *             one number to another.
- * @n: The first number.
- * @m: The second number.
- *
- * Return: The number of bits to flip.
- */
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
-{
-	unsigned long int xor_result = n ^ m;
-	unsigned int count = 0;
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int _atoi(const char *s);
+int _putchar(char c);
+int get_endianness(void);
 
-	while (xor_result)
-	{
-		count += xor_result & 1;
-		xor_result >>= 1;
-	}
-	return (count);
-}
+#endif
